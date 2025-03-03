d875e46860a4ab4b38e89f2e7f5c717be0c6dfed
dciresearch <chdanorbis@gmail.com>
2025-03-04 02:22:51 +0300

    Fix host arguments

--- run_api.py
+++ run_api.py
@@ -12,8 +12,8 @@ DETACHED_PROCESS = 0x00000008
 
 manager_command = f"uvicorn manager_server:app --port {CFG['manager_port']} --host 0.0.0.0 --reload"
 worker_command = "celery -A celery_tasks worker -l info --pool=threads --concurrency=100"
-app_command = f"uvicorn main_api:app --port {CFG['app_port']} --reload"
-dbapi_command = f"sqlite_web -p {CFG['dbapi_port']} ./database/generic.db"
+app_command = f"uvicorn main_api:app --port {CFG['app_port']} --host 0.0.0.0 --reload"
+dbapi_command = f"sqlite_web -p {CFG['dbapi_port']} --host 0.0.0.0 ./database/generic.db"
 
 
 children = []
